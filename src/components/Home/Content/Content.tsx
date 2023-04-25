21dc5e0db84f18dad4021d965110bbe23ff488fa
Лёвин Николай Сергеевич <151705@grpz.su>
2023-04-25 08:26:37 +0300

    add modal

--- src/components/Home/Content/Content.tsx
+++ src/components/Home/Content/Content.tsx
@@ -1,13 +1,39 @@
-import { Button, Calendar, Layout } from 'antd'
-import React from 'react'
+import { Button, Calendar, Layout, Modal } from 'antd'
+import React, { useState } from 'react'
 
 const Content = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleOk = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
   return (
     <Layout.Content style={{ padding: '0 20px' }}>
       <Calendar />
       <div className='my-3 text-center'>
-        <Button type='default'>Создать</Button>
+        <Button type='primary' onClick={showModal}>
+          Создать
+        </Button>
       </div>
+      <Modal
+        title='Basic Modal'
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </Layout.Content>
   )
 }
